7686c2b16544d99a6288a90e51c6ec939e3d3aa4
Joao Stephan <joaossmauricio@gmail.com>
2023-10-01 16:28:46 -0300

    solzin funfa

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,22 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Rigidbody2D))]
+
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float acceleration;
     private float speedX;
     private float speedY;
 
-    private Rigidbody playerRb;
+    private Rigidbody2D playerRb;
 
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>();
+        playerRb = GetComponent<Rigidbody2D>();
         acceleration = 10.0f;
         speedX = 0;
-        speedX = 0;
+        speedY = 0;
     }
 
     // Update is called once per frame
@@ -27,5 +28,15 @@ public class PlayerController : MonoBehaviour
         speedY = Input.GetAxis("Vertical") * acceleration;
 
         playerRb.AddForce(new Vector2(speedX, speedY));
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Sun"))
+        {
+            float distance = Vector2.Distance(transform.position, other.transform.position);
+            Vector2 direction = (transform.position - other.transform.position).normalized;
+            playerRb.AddForce(-direction / distance * distance * 100);
+        }
     }
 }
